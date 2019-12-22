1dea1fd27c7d7768e116b671b5117ad78ba5d557
JuliaSophiaWagner <julia.s.wagner@gmx.at>
2019-12-22 21:25:55 +0100

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,69 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
+//works 
 
-var paths = {
-    tsSrc: '*.ts',
-    tsDist: 'scripts'
-}
+var gulp = require('gulp');
+var typescript = require('gulp-typescript');
 
-gulp.task('ts', function () {
-    return gulp.src(paths.tsSrc)
-            .pipe(ts({
-            target: "es2015",
-            noImplicitAny: true,
-            outFile: 'index.js'
-        }))
-        .pipe(gulp.dest(paths.tsDist));
+gulp.task('typescript', function() {  
+    return gulp.src('*.ts')
+    .pipe(typescript({
+        module: 'system',   
+        target:'ES2015' 
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('compile', gulp.parallel(['ts']));
+
+// didn't work
+// Ich hab den Code unten nicht komplett zum rennen gebracht und habe immer den den error "try npm install gulp" bekommen
+
+// const { src, dest, parallel } = require('gulp');
+// const htmlhint = require('gulp-htmlhint');
+// const debug = require('gulp-debug');
+// const eslint = require('gulp-eslint');
+// const uncomment = require('gulp-uncomment');
+// var tslint = require('gulp-tslint');
+
+// async function tsLint() {
+//     return src('*.ts')
+//         .pipe(tslint({
+//             formatter: 'verbose'
+//         }))
+//         .pipe(tslint.report())
+//         .pipe(dest('./dist'));
+// }
+
+// async function html() {
+//     return src('*.html')
+//         .pipe(debug({ title: 'html:' }))
+//         .pipe(htmlhint())
+//         .pipe(htmlhint.failOnError())
+//         .pipe(dest('./dist'));
+// }
+
+// async function unComment() {
+//     return src('*.js')
+//         .pipe(uncomment({
+//             removeEmptyLines: true
+//         }))
+//         .pipe(dest('./dist'));
+// }
+
+// async function copyLibs() {
+//     return src('css/.css')
+//         .pipe(debug({ title: 'dep :' }))
+//         .pipe(dest('./dist'));
+// }
+
+// async function esLint() {
+//     return src(['ts/*.js'])
+//         .pipe(eslint())
+//         .pipe(eslint.format())
+//         .pipe(eslint.failAfterError());
+// }
+
+// exports.tslint = tsLint;
+// exports.eslint = esLint;
+// exports.html = html;
+// exports.uncomment = unComment;
+// exports.default = parallel(html, eslint, tslint, unComment);
+// exports.default = parallel(typeScript);
